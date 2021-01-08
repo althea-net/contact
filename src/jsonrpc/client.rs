d7ec0cd012a9512210d89bcfaaeafc2841add6ad
Justin Kilpatrick <justin@althea.net>
2021-01-08 16:07:13 -0500

    Fix clippy nit

--- src/jsonrpc/client.rs
+++ src/jsonrpc/client.rs
@@ -40,10 +40,7 @@ impl HTTPClient {
         // the response payload size limit for this request, almost everything
         // will set this to None, and get the default 64k, but some requests
         // need bigger buffers (like full block requests)
-        let limit = match request_size_limit {
-            Some(val) => val,
-            None => 65536,
-        };
+        let limit = request_size_limit.unwrap_or(65536);
         let url_with_method = format!("{}/{}", self.url, method);
         // if we don't have a payload this is a get request
         let res = if let Some(params) = params {
